945dbda08f9c043a316a72b0fda5126d7c1e08a1
chem1sto <vladvasiliev52@gmail.com>
2025-05-28 11:13:24 +0300

    fixed: config.py, requirements.txt

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """Модуль для настройки веб-приложения."""
 
-from constants import (
+from app.constants import (
     FLASK_DEBUG,
     FLASK_DEFAULT_WEB_APP,
     FLASK_RUN_PORT,
@@ -10,9 +10,6 @@ from constants import (
 ERROR_PAGE_404 = "404.html"
 ERROR_PAGE_505 = "505.html"
 MAIN_PAGE = "/pages/index.html"
-ROOM_PAGE = "/pages/room.html"
-START_GAME_COUNTDOWN = "/pages/start_game_countdown.html"
-WELCOME_TO_THE_NEW_GAME = "/pages/welcome_to_the_new_game.html"
 
 
 class Config(object):
